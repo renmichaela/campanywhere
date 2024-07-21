8c76ce521adc1b103f1a263012f4f6b1cd2bb5d2
Jake Suellentrop <jakesuellentrop@gmail.com>
2024-07-20 19:04:04 -0500

    chore: Update API URLs in backend/urls.py to include 'api/' prefix

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -57,7 +57,7 @@ router.register(r'expenses', ExpenseViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('api', include(router.urls)),
+    path('api/', include(router.urls)),
     path("admin/", admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
