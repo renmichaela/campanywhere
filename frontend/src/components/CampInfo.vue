db25aa6e7d490c4ee4530652fd8ade2c173ee940
Jake Suellentrop <jakesuellentrop@gmail.com>
2024-07-28 17:40:08 -0500

    chore: Update CampInfo.vue with festival schedule information

--- frontend/src/components/CampInfo.vue
+++ frontend/src/components/CampInfo.vue
@@ -38,7 +38,46 @@ import CommunityIcon from './icons/IconCommunity.vue'
       <template #heading>Line Up day</template>
 
       <ul>
-        <li></li>
+        <li>Starts 7am Saturday, Sept 7</li>
+        <li>Arrive during assigned time slot</li>
+        <li>Check in with WVF personel</li>
+        <li>Put tarp(s) down in assigned row</li>
+      </ul>
+    </ListItem>
+    <ListItem>
+      <template #icon>
+        <CommunityIcon />
+      </template>
+      <template #heading>Land Rush</template>
+
+      <ul>
+        <li>Starts 7am Thursday, Sept 12</li>
+        <li>Arrive evening before and camp at our tarps from Line Up day</li>
+        <li>Process into the campgrounds and claim our spot</li>
+        <li>Typically purchase 1 or 2 more camping spots to account for the size of our space</li>
+      </ul>
+    </ListItem>
+    <ListItem>
+      <template #icon>
+        <CommunityIcon />
+      </template>
+      <template #heading>Pre-Festival Weekend</template>
+
+      <ul>
+        <li>Many musicians and festival goers have moved in for the festival by this point</li>
+        <li>No official stages; Low key, lots of tent jams</li>
+        <li>You must have a Full Festival pass to attend</li>
+      </ul>
+    </ListItem>
+    <ListItem>
+      <template #icon>
+        <CommunityIcon />
+      </template>
+      <template #heading>Walnut Valley Festival</template>
+
+      <ul>
+        <li>Festival officially starts Wednesday, Sept 18 and goes until Sunday, Sept 22</li>
+        <li>Don't worry about where to camp, you can just Camp Anywhere!</li>
       </ul>
     </ListItem>
   </div>
